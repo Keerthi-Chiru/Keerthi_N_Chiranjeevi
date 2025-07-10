9df8b621959216d0f0786bbafa12f2beffe05af6
Keerthi N Chiranjeevi <keerthi05.niru@gmail.com>
2025-07-11 00:18:04 +0530

    Update Problem-2.js

--- Problem-2.js
+++ Problem-2.js
@@ -1,6 +1,10 @@
 const x = Number(prompt("Enter the value of x: "));
 let counter = 1;
+if( x == 1){
+    console.log(x)
+}else{
 for(let i = 1; i<x; i++){
     console.log(counter);
     counter += 2;
+}
 }
